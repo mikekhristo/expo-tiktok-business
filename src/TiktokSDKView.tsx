82481f677b4c44c3499146bd6a81704c2e878f4d
Lior Levy <levy.lior.pro@gmail.com>
2025-01-25 08:25:46 +0100

    chore: fix view

--- src/TiktokSDKView.tsx
+++ src/TiktokSDKView.tsx
@@ -1,11 +1,8 @@
 import { requireNativeView } from "expo";
 import * as React from "react";
 
-import { TiktokSDKViewProps } from "./TiktokSDK.types";
+const NativeView: React.ComponentType<any> = requireNativeView("TiktokSDK");
 
-const NativeView: React.ComponentType<TiktokSDKViewProps> =
-  requireNativeView("TiktokSDK");
-
-export default function TiktokSDKView(props: TiktokSDKViewProps) {
-  return <NativeView {...props} />;
+export default function TiktokSDKView() {
+  return <NativeView />;
 }
